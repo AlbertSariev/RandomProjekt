be2f0067f25fe6bbeaac5bfb0379a4362d2697ba
Albert Sariev <AlbertSariev@outlook.com>
2020-10-14 16:29:23 +0300

    use template

---
+++ Data/RandomProjekt.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace RandomProjekt.Data.Models
+{
+    using RandomProjekt.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
