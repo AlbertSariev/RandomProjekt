fa36ea3929b46682df51dc931b4aa2c1cf166417
AlbertSariev <72103603+AlbertSariev@users.noreply.github.com>
2021-03-19 00:17:23 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Project.View;
+using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display display = new Display();
+            display.Input();
+            ;
+        }
+    }
+}
